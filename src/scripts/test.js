b999cec5de28e4d87ec1bf6ee6c1cfd434fd6245
arthur-chueng <arthur.cheung.au@gmail.com>
2015-11-17 13:10:51 +1100

    Initial commit after setup.
    Has some errors as dojo css libraries imported directly. They were not working hotlinked.

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,5 +1,15 @@
-function testTest(){
-    console.log('Test file loaded.')
-    return 1;
-}
-testTest();
+var node = document.querySelector('.tableNode.jsTable');
+var table = new LightWeightTable (node,
+    // Definition
+    [{id: 'column1', display: 'Column 1', type: 'string'}, {id: 'column2', display: 'Column 2', type: 'string'},{id: 'column3', display: 'Column 3', type: 'string'}],
+    // Data
+    [{column1: 'cat', column2: 'dog', column3: 'pineapple', column4: 'basketball'},
+        {column1: 'bird', column2: 'hedgehog', column3: 'pineapple', column4: 'basketball'},
+        {column1: 'lion', column2: 'rag', column3: 'pineapple', column4: 'basketball'},
+        {column1: 'scissors', column2: 'rock', column3: 'paper', column4: 'dog'},
+        {column1: 'cat', column2: 'dog', column3: 'pineapple', column4: 'basketball'}
+//        {_singleColumnRow: '<div>dog<input type="text"/></div>', dog: 'name dog'}
+    ],
+    // Options
+    {includeRadio: false}
+);
